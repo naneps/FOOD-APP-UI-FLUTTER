671a9e1c11e9556ff41e7bfa1461e1f908e6bf75
naneps <ekaprasetya2244@gmail.com>
2022-04-16 20:05:21 +0700

    home screen

--- food_app_ui/lib/screens/home/home_screen.dart
+++ food_app_ui/lib/screens/home/home_screen.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:food_app_ui/constants.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Column(
+          children: [
+            Text(
+              "Delivery to".toUpperCase(),
+              style: Theme.of(context)
+                  .textTheme
+                  .caption!
+                  .copyWith(color: kActiveColor),
+            ),
+            Text(
+              "San Francisco",
+              style: TextStyle(color: Colors.blue),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
